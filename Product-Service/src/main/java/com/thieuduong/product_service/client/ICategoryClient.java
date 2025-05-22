43cd7a6b21d0420ef7d4b07ac8784b206fcf9bfb
thieu.duong <duongthieu1995@gmail.com>
2025-05-23 02:51:35 +0700

    Update ICategoryClient.java

--- Product-Service/src/main/java/com/thieuduong/product_service/client/ICategoryClient.java
+++ Product-Service/src/main/java/com/thieuduong/product_service/client/ICategoryClient.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(name = "category-service", url = "http://localhost:8082/api")
 public interface ICategoryClient {
-	@GetMapping("/category/{id}")
-	CategoryDTO getCategoryById(@PathVariable("id") int identity);
+//	@GetMapping("/category/{id}")
+//	CategoryDTO getCategoryById(@PathVariable("id") int identity);
 }
